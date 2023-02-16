98d9304dda8e0093cc9e0945d294206e4862b872
aamahin <aamahin@users.noreply.github.com>
2023-02-16 09:06:32 +0600

    Organizer wise tour list issue fixed
    In the organizer page tour list now will show of current organizer.

--- templates/single_page/organizer.php
+++ templates/single_page/organizer.php
@@ -1,1 +1,9 @@
-<?php	if ( ! defined( 'ABSPATH' ) ) {		die;	} // Cannot access pages directly.	get_header();	do_action( 'ttbm_single_organizer_page_before_wrapper' );	echo do_shortcode( "[travel-list style='modern' show='10'  pagination='yes' sidebar-filter='yes']" );	do_action( 'ttbm_single_organizer_page_after_wrapper' );	get_footer();
+<?php
+	get_header();
+	the_post();
+	$term_id = get_queried_object()->term_id;
+
+	do_action( 'ttbm_single_category_page_before_wrapper' );
+	echo do_shortcode( "[travel-list org='".$term_id."' style='modern' show='10'  pagination='yes' sidebar-filter='yes']" );
+	do_action( 'ttbm_single_category_page_after_wrapper' );
+	get_footer();

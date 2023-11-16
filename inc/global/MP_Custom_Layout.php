a599414030593c05bfdf02f9d062985ad68a9790
raselsha <raselsha@gmail.com>
2023-11-16 11:14:00 +0600

    css fix form

--- inc/global/MP_Custom_Layout.php
+++ inc/global/MP_Custom_Layout.php
@@ -92,8 +92,8 @@
 			}
 			public static function popup_button_xs($target_popup_id, $text) {
 				?>
-				<button type="button" class="bg-primary rounded p-1" data-target-popup="<?php echo esc_attr($target_popup_id); ?>">
-					<i class="fas fa-plus-square text-white"></i><span class="px-1 text-white"><?php echo esc_html($text); ?></span>
+				<button type="button" class="btn" data-target-popup="<?php echo esc_attr($target_popup_id); ?>">
+					<i class="fas fa-plus-square text-white pe-1"></i> <?php echo esc_html($text); ?>
 				</button>
 				<?php
 			}

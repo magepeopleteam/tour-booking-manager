3c3be1ec6b2fb71ffce3d4a4316730e3dc9d54bc
Mominul Islam <mominulfed@gmail.com>
2024-09-28 00:07:11 +0600

    grid layout height issue fix

--- templates/list/grid_list.php
+++ templates/list/grid_list.php
@@ -7,7 +7,7 @@
 	$thumbnail  = MP_Global_Function::get_image_url( $tour_id );
 	$term_count = 3;
 ?>
-<div class="ttbm_list_info_header">
+
     <?php include( TTBM_Function::template_path( 'layout/sale_price.php' ) ); ?>
     <div class="bg_image_area" data-href="<?php echo get_the_permalink( $tour_id ); ?>" data-placeholder>
         <div data-bg-image="<?php echo esc_attr( $thumbnail ); ?>"></div>
@@ -22,15 +22,15 @@
             <?php include( TTBM_Function::template_path( 'layout/expire_msg.php' ) ); ?>
         </div>
     </div>
-    <?php include( TTBM_Function::template_path( 'layout/list_title.php' ) ); ?>
-</div>
 
-<div class="ttbm_list_content_wrapper">
+
+
+
 <div class="fdColumn ttbm_list_details">
+    <?php include( TTBM_Function::template_path( 'layout/list_title.php' ) ); ?>
     <?php include( TTBM_Function::template_path( 'layout/location.php' ) ); ?>
     <div class="divider"></div>
     <?php include( TTBM_Function::template_path( 'layout/description_short.php' ) ); ?>
     <div class="divider"></div>
     <?php include( TTBM_Function::template_path( 'layout/list_bottom.php' ) ); ?>
-</div>
 </div>

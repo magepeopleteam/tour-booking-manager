d3b2dfa82a30ecabcab8d01c979e31a4de48bb46
ShahnurAlam <shahnurlfe@gmail.com>
2024-11-25 09:20:54 +0600

    create back to back pages---issue solved

--- tour-booking-manager.php
+++ tour-booking-manager.php
@@ -71,64 +71,78 @@
 				}
 			}
 			public static function on_activation_page_create() {
-				if (!MP_Global_Function::get_page_by_slug('find')) {
-					$ttbm_search_page = array(
-						'post_type' => 'page',
-						'post_name' => 'find',
-						'post_title' => 'Tour Search Result',
-						'post_content' => '[ttbm-search-result]',
-						'post_status' => 'publish',
-					);
-					unset($find_page_id);
-					$find_page_id = wp_insert_post($ttbm_search_page);
-					if (is_wp_error($find_page_id)) {
-						printf('<div class="error" style="background:red; color:#fff;"><p>%s</p></div>', $find_page_id->get_error_message());
-					}
-				}
-				if (!MP_Global_Function::get_page_by_slug('lotus-grid')) {
-					$ttbm_search_page = array(
-						'post_type' => 'page',
-						'post_name' => 'lotus-grid',
-						'post_title' => 'Tour Lotus Grid View',
-						'post_content' => "[travel-list style='lotus' column=4 show='12' pagination='yes']",
-						'post_status' => 'publish',
-					);
-					unset($find_page_id);
-					$find_page_id = wp_insert_post($ttbm_search_page);
-					if (is_wp_error($find_page_id)) {
-						printf('<div class="error" style="background:red; color:#fff;"><p>%s</p></div>', $find_page_id->get_error_message());
-					}
-				}
-				if (!MP_Global_Function::get_page_by_slug('orchid-grid')) {
-					$ttbm_search_page = array(
-						'post_type' => 'page',
-						'post_name' => 'orchid-grid',
-						'post_title' => 'Tour Orchid Grid View',
-						'post_content' => "[travel-list style='orchid' column=4 pagination='yes' show=12]",
-						'post_status' => 'publish',
-					);
-					unset($find_page_id);
-					$find_page_id = wp_insert_post($ttbm_search_page);
-					if (is_wp_error($find_page_id)) {
-						printf('<div class="error" style="background:red; color:#fff;"><p>%s</p></div>', $find_page_id->get_error_message());
-					}
-				}
-				if (get_option('ttbm_repeated_field_update') != 'completed') {
-					$args = array(
-						'post_type' => 'ttbm_tour',
-						'posts_per_page' => -1
-					);
-					$qr = new WP_Query($args);
-					foreach ($qr->posts as $result) {
-						$post_id = $result->ID;
-						$start_date = MP_Global_Function::get_post_info($post_id, 'ttbm_travel_start_date');
-						$end_date = MP_Global_Function::get_post_info($post_id, 'ttbm_travel_end_date');
-						update_post_meta($post_id, 'ttbm_travel_repeated_start_date', $start_date);
-						update_post_meta($post_id, 'ttbm_travel_repeated_end_date', $end_date);
-					}
-					update_option('ttbm_repeated_field_update', 'completed');
-				}
-			}
+            $pages_to_create = [
+                'find' => [
+                    'slug' => 'find',
+                    'title' => 'Tour Search Result',
+                    'content' => '[ttbm-search-result]',
+                    'option_key' => 'ttbm_page_find_created',
+                ],
+                'lotus-grid' => [
+                    'slug' => 'lotus-grid',
+                    'title' => 'Tour Lotus Grid View',
+                    'content' => "[travel-list style='lotus' column=4 show='12' pagination='yes']",
+                    'option_key' => 'ttbm_page_lotus_grid_created',
+                ],
+                'orchid-grid' => [
+                    'slug' => 'orchid-grid',
+                    'title' => 'Tour Orchid Grid View',
+                    'content' => "[travel-list style='orchid' column=4 pagination='yes' show=12]",
+                    'option_key' => 'ttbm_page_orchid_grid_created',
+                ],
+            ];
+
+            foreach ($pages_to_create as $page_data) {
+                $page_exists = MP_Global_Function::get_page_by_slug($page_data['slug']); // Check by slug
+                $option_exists = get_option($page_data['option_key']); // Check option table
+
+                if (!$page_exists && !$option_exists) {
+                    // Create the page if it doesn't exist
+                    $page = [
+                        'post_type' => 'page',
+                        'post_name' => $page_data['slug'],
+                        'post_title' => $page_data['title'],
+                        'post_content' => $page_data['content'],
+                        'post_status' => 'publish',
+                    ];
+                    $page_id = wp_insert_post($page);
+
+                    if (is_wp_error($page_id)) {
+                        printf('<div class="error" style="background:red; color:#fff;"><p>%s</p></div>', $page_id->get_error_message());
+                    } else {
+                        update_option($page_data['option_key'], true);
+                    }
+                }
+            }
+
+            // Update repeated fields
+            self::update_repeated_fields();
+        }
+
+        private static function update_repeated_fields() {
+            if (get_option('ttbm_repeated_field_update') === 'completed') {
+                return; // Skip if already updated
+            }
+
+            $args = [
+                'post_type' => 'ttbm_tour',
+                'posts_per_page' => -1,
+            ];
+
+            $query = new WP_Query($args);
+            if ($query->have_posts()) {
+                foreach ($query->posts as $post) {
+                    $post_id = $post->ID;
+                    $start_date = MP_Global_Function::get_post_info($post_id, 'ttbm_travel_start_date');
+                    $end_date = MP_Global_Function::get_post_info($post_id, 'ttbm_travel_end_date');
+                    update_post_meta($post_id, 'ttbm_travel_repeated_start_date', $start_date);
+                    update_post_meta($post_id, 'ttbm_travel_repeated_end_date', $end_date);
+                }
+            }
+
+            wp_reset_postdata();
+            update_option('ttbm_repeated_field_update', 'completed');
+        }
 			public function woocommerce_not_active() {
 				$wc_install_url = get_admin_url() . 'plugin-install.php?s=woocommerce&tab=search&type=term';
 				printf('<div class="error" style="background:red; color:#fff;"><p>%s</p></div>', __('You Must Install WooCommerce Plugin before activating Tour Booking Manager, Because It is dependent on Woocommerce Plugin. <a class="btn button" href=' . $wc_install_url . '>Click Here to Install</a>'));

51a0853be5b86c576112136b38d3c048c191910f
Shahadat <raselsha@gmail.com>
2024-12-11 12:20:50 +0600

    style: fix icon color

--- templates/layout/duration_box.php
+++ templates/layout/duration_box.php
@@ -11,8 +11,9 @@
 	if ( ( $day || $night ) && $tour_type == 'general' && MP_Global_Function::get_post_info( $ttbm_post_id, 'ttbm_display_duration', 'on' ) != 'off' ) {
 		?>
 
-			<div class="item_icon">
+			<div class="item_icon" title="<?php esc_html_e( 'Duration', 'tour-booking-manager' ); ?>">
 				<i class="fas fa-clock"></i>
+				
 				<strong>
 					<?php
 						if ( $day && $day > 1 ) {

f9f2fd35481d34ce08eadd8391054fa8d91985e8
Shahadat <raselsha@gmail.com>
2025-04-11 10:55:30 +0600

    fix: activities showing in frontend

--- templates/layout/smart_activity.php
+++ templates/layout/smart_activity.php
@@ -2,22 +2,25 @@
 	if ( ! defined( 'ABSPATH' ) ) {
 		die;
 	}
-$ttbm_post_id         = $ttbm_post_id ?? get_the_id();
+	$ttbm_post_id = $ttbm_post_id ?? get_the_ID(); // fixed get_the_id() to get_the_ID()
+
 	$tour_activities = MP_Global_Function::get_post_info( $ttbm_post_id, 'ttbm_tour_activities', array() );
-	if ( sizeof( $tour_activities ) > 0 && MP_Global_Function::get_post_info( $ttbm_post_id, 'ttbm_display_activities', 'on' ) != 'off' ) {
+	$tour_activities_array = explode(',', $tour_activities[0]);
+	$activities_terms = MP_Global_Function::get_taxonomy('ttbm_tour_activities');
+	
+	if ( ! empty( $tour_activities ) && MP_Global_Function::get_post_info( $ttbm_post_id, 'ttbm_display_activities', 'on' ) !== 'off' ) {
 		?>
-		<div class='ttbm_description'>
-			<h2><?php esc_html_e( 'Activities ', 'tour-booking-manager'); ?></h2>
+		<div class="ttbm_description">
+			<h2><?php esc_html_e( 'Activities', 'tour-booking-manager' ); ?></h2>
 			<ul class="ttbm-activities">
-				<?php foreach ( $tour_activities as $tour_activity ) {
-					$term = get_term_by( 'name', $tour_activity, 'ttbm_tour_activities' );
-					if ( $term ) {
-						$icon = get_term_meta( $term->term_id, 'ttbm_activities_icon', true );
+				<?php
+				foreach ( $activities_terms as $activity ) {
+					if ( in_array( $activity->term_id, $tour_activities_array ) ) {
 						$icon = $icon ?: 'fa fa-check';
 						?>
 						<li class="ttbm-items">
 							<i class="<?php esc_attr_e( $icon ); ?>"></i>
-							<?php esc_html_e( $term->name ); ?>
+							<?php esc_html_e( $activity->name ); ?>
 						</li>
 						<?php
 					}
@@ -25,4 +28,6 @@ $ttbm_post_id         = $ttbm_post_id ?? get_the_id();
 				?>
 			</ul>
 		</div>
-	<?php } ?>
+		<?php
+	}
+

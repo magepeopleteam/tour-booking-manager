4866ea1a4b74e1ff94013a8b5652ef82d33264fe
rubelcuet10@gmail.com <you@example.com>
2025-04-28 15:30:43 +0600

    hotel order lists shows

---
+++ admin/TTBM_Hotel_Booking_Lists.php
@@ -0,0 +1,178 @@
+<?php
+if (!defined('ABSPATH')) {
+    die;
+} // Cannot access pages directly.
+if (!class_exists('TTBM_Hotel_Booking_Lists')) {
+    class TTBM_Hotel_Booking_Lists{
+
+        public function __construct() {
+            add_action('admin_menu', array($this, 'hotel_booking_list_menu'), 1);
+        }
+
+        public function hotel_booking_list_menu() {
+
+            $label = __('Hotel Booking Lists', 'tour-booking-manager');
+            add_submenu_page(
+                'edit.php?post_type=ttbm_tour',
+                $label,
+                $label,
+                'manage_options',
+                'ttbm_hotel_booking_lists',
+                array($this, 'ttbm_hotel_order')
+            );
+
+        }
+
+        public function ttbm_hotel_order() {
+            $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+
+            $args = array(
+                'post_type'      => 'ttbm_hotel_booking',
+                'posts_per_page' => 20,
+                'paged'          => $paged,
+            );
+            $query = new WP_Query($args);
+            ?>
+
+            <div class="ttbm_total_booking_wrapper">
+                <h2 class="ttbm_total_booking_title"><?php echo esc_attr__( 'Travel Order List', 'tour-booking-manager' )?></h2>
+
+                <div class="ttbm_total_booking_filter_section">
+                    <span class="ttbm_total_booking_filter_label"><?php echo esc_attr__( 'Filter List By:', 'tour-booking-manager' )?></span>
+                    <div class="ttbm_total_booking_filter_options">
+                        <label class="ttbm_total_booking_radio_container">
+                            <input type="radio" name="filter_type" value="travel" checked>
+                            <span class="ttbm_total_booking_radio_text"><?php echo esc_attr__( 'Hotel', 'tour-booking-manager' )?></span>
+                        </label>
+                    </div>
+                    <div class="ttbm_total_booking_filter_controls">
+                        <select class="ttbm_total_booking_select">
+                            <option><?php echo esc_attr__( 'Select Travel', 'tour-booking-manager' )?></option>
+                        </select>
+                        <button class="ttbm_total_booking_filter_btn"<?php echo esc_attr__( 'Filter', 'tour-booking-manager' )?>></button>
+                        <button class="ttbm_total_booking_reset_btn"><?php echo esc_attr__( 'Reset', 'tour-booking-manager' )?></button>
+                    </div>
+                </div>
+
+                <!-- Order Summary -->
+                <div class="ttbm_total_booking_summary">
+                    <div class="ttbm_total_booking_found">Total <span><?php echo $query->found_posts; ?></span><?php echo esc_attr__( 'Order Found', 'tour-booking-manager' )?></div>
+                    <div class="ttbm_total_booking_showing">Showing <span><?php echo $query->post_count; ?></span><?php echo esc_attr__( 'Order', 'tour-booking-manager' )?> </div>
+                    <div class="ttbm_total_booking_per_page">
+                        <span><?php echo esc_attr__( 'Guest Per Page', 'tour-booking-manager' )?></span>
+                        <input type="number" value="20" class="ttbm_total_booking_page_input">
+                    </div>
+                </div>
+
+                <!-- Orders Table -->
+                <table class="ttbm_total_booking_table">
+                    <thead class="ttbm_total_booking_thead">
+                    <tr>
+                        <th class="ttbm_total_booking_th"><?php echo esc_attr__( 'Sl.', 'tour-booking-manager' )?></th>
+                        <th class="ttbm_total_booking_th"><?php echo esc_attr__( 'Order ID', 'tour-booking-manager' )?></th>
+                        <th class="ttbm_total_booking_th"><?php echo esc_attr__( 'Billing Information', 'tour-booking-manager' )?></th>
+                        <th class="ttbm_total_booking_th"><?php echo esc_attr__( 'Hotel', 'tour-booking-manager' )?></th>
+                        <th class="ttbm_total_booking_th"><?php echo esc_attr__( 'Total Days', 'tour-booking-manager' )?></th>
+                        <th class="ttbm_total_booking_th"><?php echo esc_attr__( 'Rooms', 'tour-booking-manager' )?></th>
+                        <th class="ttbm_total_booking_th"><?php echo esc_attr__( 'Check In Date', 'tour-booking-manager' )?></th>
+                        <th class="ttbm_total_booking_th"><?php echo esc_attr__( 'Check Out Date', 'tour-booking-manager' )?></th>
+                        <th class="ttbm_total_booking_th"><?php echo esc_attr__( 'Order Date', 'tour-booking-manager' )?></th>
+                        <th class="ttbm_total_booking_th"><?php echo esc_attr__( 'Order Status', 'tour-booking-manager' )?></th>
+                        <th class="ttbm_total_booking_th"><?php echo esc_attr__( 'Paid Amount', 'tour-booking-manager' )?></th>
+                        <th class="ttbm_total_booking_th"><?php echo esc_attr__( 'Payment Method', 'tour-booking-manager' )?></th>
+<!--                        <th class="ttbm_total_booking_th">Action</th>-->
+                    </tr>
+                    </thead>
+                    <tbody class="ttbm_total_booking_tbody">
+
+                    <?php
+                    if ( $query->have_posts() ) :
+                        $sl = 1 + ( ( $paged - 1 ) * 20 );
+                        while ( $query->have_posts() ) : $query->the_post();
+                            $order_id = get_post_meta(get_the_ID(), '_ttbm_hotel_booking_order_id', true);
+                            $billing_name = get_post_meta(get_the_ID(), '_ttbm_hotel_booking_customer_name', true);
+                            $travel_name = get_post_meta(get_the_ID(), '_ttbm_hotel_title', true);
+                            $booking_days = get_post_meta(get_the_ID(), '_ttbm_hotel_booking_days', true);
+                            $hotel_infos = get_post_meta(get_the_ID(), '_ttbm_hotel_booking_room_info', true);
+                            $check_in = get_post_meta(get_the_ID(), '_ttbm_hotel_booking_checkin_date', true);
+                            $check_in = date('F j, Y', strtotime($check_in));
+                            $check_out = get_post_meta(get_the_ID(), '_ttbm_hotel_booking_checkout_date', true);
+                            $check_out = date('F j, Y', strtotime($check_out));
+                            $order_date = get_the_date('F j, Y');
+                            $order_status = get_post_meta(get_the_ID(), '_ttbm_hotel_booking_status', true);
+                            $paid_amount = get_post_meta(get_the_ID(), '_ttbm_hotel_booking_price', true);
+                            $payment_method = get_post_meta(get_the_ID(), '_ttbm_hotel_booking_payment_method', true);
+                            ?>
+
+                            <tr class="ttbm_total_booking_tr">
+                                <td class="ttbm_total_booking_td"><?php echo $sl++; ?></td>
+                                <td class="ttbm_total_booking_td ttbm_total_booking_order_id">#<?php echo esc_html($order_id); ?></td>
+                                <td class="ttbm_total_booking_td ttbm_total_booking_billing">
+                                    <?php echo esc_html($billing_name); ?>
+                                    <button class="ttbm_total_booking_view_more"><?php echo esc_attr__( 'View More', 'tour-booking-manager' )?></button>
+                                </td>
+                                <td class="ttbm_total_booking_td ttbm_total_booking_travel">
+                                    <?php echo esc_html($travel_name); ?>
+                                </td>
+                                <td class="ttbm_total_booking_td ttbm_total_booking_qty"><?php echo esc_html( $booking_days ); ?></td>
+                                <td class="ttbm_total_booking_td ttbm_total_booking_ticket">
+                                    <?php
+                                    if( is_array( $hotel_infos ) && !empty( $hotel_infos ) ) {
+                                        $hotel_info_count = count( $hotel_infos );
+                                        if( $hotel_info_count > 1 ) {
+                                            $comma = ',';
+                                        }else{
+                                            $comma = '';
+                                        }
+                                        foreach ($hotel_infos as $room_name => $room_infos ) { ?>
+                                            <div class="ttbm_booking_room_data"><?php echo esc_attr( $room_name )?>(<?php echo esc_attr( $room_infos['quantity'])?>)</div>
+                                        <?php
+                                        }
+                                    }
+
+                                    ?>
+                                </td>
+                                <td class="ttbm_total_booking_td ttbm_total_booking_tour_date"><?php echo esc_html($check_in); ?></td>
+                                <td class="ttbm_total_booking_td ttbm_total_booking_tour_date"><?php echo esc_html($check_out); ?></td>
+                                <td class="ttbm_total_booking_td ttbm_total_booking_order_date"><?php echo esc_html($order_date); ?></td>
+                                <td class="ttbm_total_booking_td ttbm_total_booking_status"><?php echo esc_html($order_status); ?></td>
+                                <td class="ttbm_total_booking_td ttbm_total_booking_amount"><?php echo esc_html($paid_amount); ?>৳</td>
+                                <td class="ttbm_total_booking_td ttbm_total_booking_payment"><?php echo esc_html($payment_method); ?></td>
+                                <!--<td class="ttbm_total_booking_td ttbm_total_booking_action">
+                                    <button class="ttbm_total_booking_action_btn">
+                                        <i class="ttbm_total_booking_icon"></i>
+                                    </button>
+                                </td>-->
+                            </tr>
+
+                        <?php endwhile;
+                        wp_reset_postdata();
+                    else : ?>
+                        <tr><td colspan="14"><?php echo esc_attr__( 'No bookings found.', 'tour-booking-manager' )?></td></tr>
+                    <?php endif; ?>
+
+                    </tbody>
+                </table>
+
+                <!-- Pagination -->
+                <div class="ttbm_total_booking_pagination">
+                    <?php
+                    echo paginate_links(array(
+                        'total' => $query->max_num_pages,
+                        'current' => $paged,
+                        'prev_text' => __('« Prev'),
+                        'next_text' => __('Next »'),
+                    ));
+                    ?>
+                </div>
+            </div>
+
+            <?php
+        }
+
+
+
+    }
+
+    new TTBM_Hotel_Booking_Lists();
+}
--- inc/TTBM_Hotel_Booking.php
+++ inc/TTBM_Hotel_Booking.php
@@ -18,6 +18,154 @@ if ( ! class_exists( 'TTBM_Hotel_Booking' ) ) {
             add_action('woocommerce_before_calculate_totals', [$this, 'update_cart_item_price'], 10, 1);
             add_filter('woocommerce_get_item_data', [$this, 'display_custom_cart_item_data'], 10, 2);
             add_action('woocommerce_order_item_meta_end', [$this, 'display_order_meta'], 10, 3);
+            add_action('woocommerce_checkout_create_order_line_item', [$this, 'add_order_item_meta'], 10, 4);
+            add_action('woocommerce_new_order', [$this, 'mptrs_woocommerce_new_order'], 10, 1);
+
+            add_action( 'woocommerce_order_status_changed', [$this, 'custom_function_on_order_status_change'], 10, 4 );
+
+
+        }
+
+
+        function custom_function_on_order_status_change( $order_id, $old_status, $new_status, $order ) {
+
+
+            if ( $new_status === 'processing' ) {
+                $orderPostId = '';
+                foreach ( $order->get_items() as $item_id => $item ) {
+                    $product = $item->get_product();
+                    $orderPostId = $product->get_id();
+                }
+                $ttbm_booking_data = maybe_unserialize( get_post_meta( $orderPostId, '_ttbm_hotel_booking_data', true ) );
+
+                if( isset( $ttbm_booking_data['hotel_booking'] ) ){
+
+                    $payment_method = $order->get_payment_method_title();
+
+                    $order_description = isset( $ttbm_booking_data['hotel_room_ordered_data_info'] ) ? $ttbm_booking_data['hotel_room_ordered_data_info'] : [];
+                    $checkin_date = isset( $ttbm_booking_data['ttbm_hotel_info']['ttbm_checkin_date'] ) ? $ttbm_booking_data['ttbm_hotel_info']['ttbm_checkin_date'] : '';
+                    $checkout_date = isset( $ttbm_booking_data['ttbm_hotel_info']['ttbm_checkout_date'] ) ? $ttbm_booking_data['ttbm_hotel_info']['ttbm_checkout_date'] : '';
+                    $hotel_id = isset( $ttbm_booking_data['ttbm_hotel_info']['hotel_id'] ) ? $ttbm_booking_data['ttbm_hotel_info']['hotel_id'] : '';
+                    $number_of_days = isset( $ttbm_booking_data['booking_days'] ) ? $ttbm_booking_data['booking_days'] : '';
+                    $price = isset( $ttbm_booking_data['price'] ) ? $ttbm_booking_data['price'] : 0;
+
+                    $hotel_title = get_the_title( $hotel_id );
+
+                    $hotel_booking_status = 'In Progress';
+                    $order_title = 'Hotel Booking #' . $order_id;
+                    $order_created_date = $order->get_date_created()->date('Y-m-d H:i:s');
+                    $order_status = $new_status;
+                    $customer_id = $order->get_customer_id();
+                    $customer_name = $order->get_billing_first_name() . ' ' . $order->get_billing_last_name();
+                    $customer_email = $order->get_billing_email();
+
+                    $custom_order_id = wp_insert_post(array(
+                        'post_title'    => $order_title,
+                        'post_type'     => 'ttbm_hotel_booking',
+                        'post_status'   => 'publish',
+                        'post_author'   => 1,
+                    ));
+
+                    if ( $custom_order_id ) {
+                        // Store meta data in the custom post
+                        update_post_meta($custom_order_id, '_ttbm_hotel_booking_order_id', $order_id);
+                        update_post_meta($custom_order_id, '_ttbm_hotel_booking_date', $order_created_date);
+                        update_post_meta($custom_order_id, '_ttbm_hotel_booking_status', $order_status);
+                        update_post_meta($custom_order_id, '_ttbm_hotel_booking_customer_id', $customer_id);
+                        update_post_meta($custom_order_id, '_ttbm_hotel_booking_customer_name', $customer_name);
+                        update_post_meta($custom_order_id, '_ttbm_hotel_booking_customer_email', $customer_email);
+                        update_post_meta($custom_order_id, '_ttbm_hotel_booking_room_info', $order_description);
+                        update_post_meta($custom_order_id, '_ttbm_hotel_booking_checkin_date', $checkin_date);
+                        update_post_meta($custom_order_id, '_ttbm_hotel_booking_checkout_date', $checkout_date);
+                        update_post_meta($custom_order_id, '_ttbm_hotel_booking_status', $hotel_booking_status);
+                        update_post_meta($custom_order_id, '_ttbm_hotel_booking_days', $number_of_days);
+                        update_post_meta($custom_order_id, '_ttbm_hotel_booking_price', $price);
+                        update_post_meta($custom_order_id, '_ttbm_hotel_id', $hotel_id);
+                        update_post_meta($custom_order_id, '_ttbm_hotel_title', $hotel_title);
+                        update_post_meta($custom_order_id, '_ttbm_hotel_booking_payment_method', $payment_method);
+                    }
+                }
+
+            }
+        }
+        public function mptrs_woocommerce_new_order( $order_id ) {
+            $order = wc_get_order( $order_id );
+            $orderPostId = '';
+            foreach ( $order->get_items() as $item_id => $item ) {
+                $product = $item->get_product();
+                $orderPostId = $product->get_id();
+            }
+
+            if( !empty( $orderPostId ) ){
+                $mptrs_booking_data = maybe_unserialize( get_post_meta( $orderPostId, '_ttbm_hotel_booking_data', true ) );
+                if( is_array( $mptrs_booking_data ) && !empty( $mptrs_booking_data ) ) {
+                    $hotel_id = $mptrs_booking_data['ttbm_hotel_info']['hotel_id'];
+                    $check_in = $mptrs_booking_data['ttbm_hotel_info']['ttbm_checkin_date'];
+                    $check_out = $mptrs_booking_data['ttbm_hotel_info']['ttbm_checkout_date'];
+
+                    $room_data_info = $mptrs_booking_data['hotel_room_ordered_data_info'];
+                    $booking_request = [];
+                    if( !empty( $room_data_info ) ) {
+                        foreach ($room_data_info as $room_type => $info) {
+                            if (!empty($info['quantity'])) {
+                                $booking_request[$room_type] = $info['quantity'];
+                            }
+                        }
+                    }
+                    if( !empty( $booking_request ) ) {
+                        MP_Global_Function::pa_add_multiple_room_type_booking( $hotel_id, $booking_request, $check_in, $check_out);
+                    }
+
+                }
+
+            }
+
+        }
+
+        function add_order_item_meta(  $item, $cart_item_key, $values, $order ){
+
+
+
+            if( isset( $values['ttbm_hotel_booking']) ) {
+
+                $meta_fields = [
+                    'description_order' => __('Hotel Booking Details', 'tour-booking-manager') ,
+                    'booking_seats' => __( 'Seats', 'tour-booking-manager'),
+                    'checkin_date' => __('Check In Date', 'tour-booking-manager'),
+                    'checkout_date' => __( 'Check Out Date', 'tour-booking-manager' ),
+                ];
+
+                foreach ($meta_fields as $key => $label) {
+                    if (!empty($values[$key])) {
+                        $value = is_array($values[$key]) ? implode(', ', $values[$key]) : $values[$key];
+                        $item->add_meta_data($label, $value, true);
+                    }
+                }
+
+                $customer_name = trim($order->get_billing_first_name() . ' ' . $order->get_billing_last_name());
+                if (!empty($customer_name)) {
+                    $item->add_meta_data('Customer Name', $customer_name, true);
+                }
+            }
+
+            if( isset( $values['ttbm_hotel_booking'] ) ){
+                $order_data = [];
+                $product_id = isset( $values['ttbm_hotel_id'] ) ? $values['ttbm_hotel_id'] : '';
+                $order_data = array(
+                    'hotel_room_ordered_data_info' => $values['room_ordered_data_info'],
+                    'price' => $values['price'],
+                    'ttbm_hotel_info' => $values['ttbm_hotel_info'],
+                    'quantity' => $values['quantity'],
+                    'booking_days' => $values['days'],
+                    'hotel_booking' => $values['ttbm_hotel_booking'],
+                );
+
+                $order_data = maybe_serialize( $order_data );
+                if( !empty( $product_id ) ){
+                    update_post_meta( $product_id, '_ttbm_hotel_booking_data', $order_data );
+                }
+
+            }
 
         }
 
@@ -36,7 +184,12 @@ if ( ! class_exists( 'TTBM_Hotel_Booking' ) ) {
             $date_range = $_REQUEST['date_range'] ?? "";
             $room_data_info = json_decode( wp_unslash( $_REQUEST['room_data_info'] ), true );
 
+
             $room_output = "Room List\n\n";
+
+            $currency_symbol = get_woocommerce_currency_symbol();
+            $room_output .= '<div class="ttbm_hotel_ordered_room_list">';
+
             foreach ($room_data_info as $roomName => $info) {
                 $qty = $info['quantity'];
                 $price = $info['price'];
@@ -45,10 +198,15 @@ if ( ! class_exists( 'TTBM_Hotel_Booking' ) ) {
                 // Format room name: insert space before capital letters except first
                 $formattedRoomName = preg_replace('/(?<!^)([A-Z])/', ' $1', $roomName);
 
-                $room_output .= "Room Name :    {$formattedRoomName}\n";
-                $room_output .= "Qty :  {$qty}\n";
-                $room_output .= "Price :  ( " . number_format($price, 2) . "৳ x {$qty} x 1 ) = " . number_format($total, 2) . "৳\n\n";
+                // Build the output
+                $room_output .= " <div class='ttbm_hotel_room'>";
+                $room_output .= " <div class='ttbm_hotel_room_name'> Room Name: {$formattedRoomName}</div> ";
+                $room_output .= " <div class='ttbm_hotel_qty'>Qty: {$qty}</div> ";
+                $room_output .= " <div class='ttbm_hotel_price'>Price: ( " . number_format($price, 2) . " {$currency_symbol} x {$qty} x 1 ) = " . number_format($total, 2) . " {$currency_symbol}</div> ";
+                $room_output .= " </div> ";
             }
+
+            $room_output .= '</div>';
 
             $date       = explode( "    -    ", $date_range );
             $check_in = date( 'Y-m-d', strtotime( $date[0] ) );
@@ -56,16 +214,6 @@ if ( ! class_exists( 'TTBM_Hotel_Booking' ) ) {
 
             $days = 2;
 
-            $booking_request = [];
-            if( !empty( $room_data_info ) ) {
-                foreach ($room_data_info as $room_type => $info) {
-                    if (!empty($info['quantity'])) {
-                        $booking_request[$room_type] = $info['quantity'];
-                    }
-                }
-            }
-
-            error_log( print_r( [ '$booking_request' => $booking_request ], true ) );
 
             $post_id = get_post_meta( $hotel_id, 'link_wc_product', true);
             $price = $_REQUEST['price'] ?? 0;
@@ -80,6 +228,7 @@ if ( ! class_exists( 'TTBM_Hotel_Booking' ) ) {
                 'ttbm_hotel_booking' => 'yes',
                 'ttbm_hotel_id' => $post_id,
                 'description_order' => $room_output,
+                'room_ordered_data_info' => $room_data_info,
                 'price' => $price,
                 'ttbm_tp' => $price,
                 'line_total' => $price,
@@ -94,9 +243,7 @@ if ( ! class_exists( 'TTBM_Hotel_Booking' ) ) {
                 wp_send_json_error('WooCommerce is not active.');
             }
 
-            MP_Global_Function::pa_add_multiple_room_type_booking( $hotel_id, $booking_request, $check_in, $check_out);
             WC()->cart->empty_cart();
-
             $cart_item_key = WC()->cart->add_to_cart( $post_id, $quantity, 0, [], $cart_item_data );
             if ($cart_item_key) {
                 wp_send_json_success('Item added to cart.');
@@ -140,7 +287,7 @@ if ( ! class_exists( 'TTBM_Hotel_Booking' ) ) {
                     $value = is_array($cart_item[$key]) ? implode(', ', $cart_item[$key]) : $cart_item[$key];
                     $item_data[] = [
                         'name'  => $label,
-                        'value' => esc_html(esc_html( $value ) )
+                        'value' => wp_kses_post( $value ),
                     ];
                 }
             }

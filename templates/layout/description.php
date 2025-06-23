2897ad3c0bb054cb9ef62a0302069f11c6fc7ad2
Shahadat <raselsha@gmail.com>
2025-06-23 09:54:06 +0600

    fix: translation string

--- templates/layout/description.php
+++ templates/layout/description.php
@@ -5,6 +5,6 @@
 	$ttbm_post_id = $ttbm_post_id ?? get_the_id();
 		?>
 		<div class="ttbm_description ttbm_wp_editor" data-placeholder="">
-			<h2><?php _e('Overview','tour-booking-manager'); ?></h2>
+			<h2><?php esc_html_e('Overview','tour-booking-manager'); ?></h2>
 			<?php the_content(); ?>
 		</div>

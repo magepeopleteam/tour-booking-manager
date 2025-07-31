9316a05a6b98163d08cb16dd0ca8771881f4b9c2
Shahadat <raselsha@gmail.com>
2025-07-31 11:24:10 +0600

    fix: slider below icon

--- templates/layout/start_location_box.php
+++ templates/layout/start_location_box.php
@@ -10,7 +10,7 @@
 		?>
 		
 			<div class="item_icon" title="<?php esc_html_e( 'Start Location', 'tour-booking-manager' ); ?>">
-				<i class="fas fa-map-marker"></i>
+				<i class="mi mi-location"></i>
 				<?php echo esc_html( $start_place ); ?>
 			</div>
 

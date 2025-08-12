ba4f66c217cde18f2dbe6db64de7efa925497c63
Shahadat <raselsha@gmail.com>
2025-08-12 14:20:53 +0600

    feat: hotel hero aria updated

--- templates/themes/hotel_default.php
+++ templates/themes/hotel_default.php
@@ -8,48 +8,80 @@ $ttbm_post_id = $ttbm_post_id ?? get_the_id();
 $tour_id=$tour_id??TTBM_Function::post_id_multi_language($ttbm_post_id);
 $class_location = $class_location ?? '';
 ?>
-    <div class="ttbm_default_theme">
-        <div class='ttbm_style ttbm_wraper'>
-            <div class="ttbm_hotel_item fdColumn">
-                <div class="ttbm_container">
-                <div class="ttbm_details_page">
-                    <div class="ttbm_details_page_header">
-                        <?php do_action( 'ttbm_details_title' ); ?>
-                        <div class="dFlex justifyStart">
-                            <?php do_action( 'ttbm_details_title_after', $ttbm_post_id ); ?>
-                            <?php
-                            if ( is_plugin_active( 'tour-booking-manager-pro/tour-booking-manager-pro.php' ) ):
-                                $location = TTBM_Function::get_full_location( $ttbm_post_id );
-                                if ( $location && TTBM_Global_Function::get_post_info( $ttbm_post_id, 'ttbm_display_hotel_location', 'on' ) != 'off' ) {
-                                    ?>
-                                    <span class="pL_xs pR_xs">|</span>
-                                <?php }
-                            endif;
-                            ?>
-                            <?php include( TTBM_Function::template_path( 'layout/location.php' ) ); ?>
-                        </div>
-
-                        <div class="ttbm_content_area ttbm_hotel_content_area">
-                            <input type="hidden" id="ttbm_booking_hotel_id" value="<?php echo esc_attr( $ttbm_post_id )?>">
-                            <div class="ttbm_content__left">
-                                <?php do_action( 'ttbm_hotel_slider' ); ?>
-
-                                <?php do_action( 'ttbm_make_hotel_booking', $ttbm_post_id );?>
-
-                                <?php do_action( 'ttbm_description' ); ?>
-                            </div>
-                            <div class="ttbm_content__right">
-                                <?php do_action( 'ttbm_hotel_location_details' ); ?>
-                                <?php do_action( 'ttbm_include_feature' ); ?>
-                                <?php do_action( 'ttbm_exclude_service' ); ?>
-                            </div>
-                        </div>
-
+<div class="ttbm_default_theme">
+    <div class='ttbm_style'>
+        <div class="ttbm-hotel-area ">
+            <div class="ttbm-hero-header">
+                <div class="title-section">
+                    <span class="review-star">
+                        <i class="mi mi-star"></i>
+                        <i class="mi mi-star"></i>
+                        <i class="mi mi-star"></i>
+                        <i class="mi mi-star"></i>
+                        <i class="mi mi-star"></i>
+                    </span>
+                    <h1 class="title"><?php the_title(); ?></h1>
+                    <p class="location-info">
+                        <i class="mi mi-marker"></i> 
+                        <?php echo __('197/2 Moo 1, Tambol Pongyang, Amphur Mae Rim, 50180 Mae Rim, Thailand','tour-booking-manager'); ?>
+                        <a href="#"><?php echo __('Great location - show map','tour-booking-manager'); ?></a>
+                    </p>
+                </div>
+                <div class="sharing-meta">
+                    <div class="sharing-info">
+                        <span>
+                            <i class="mi mi-heart"></i>
+                            <i class="mi mi-share"></i>
+                        </span>
+                        <button><?php echo __('Reserve','tour-booking-manager'); ?></button>
+                    </div>
+                    <div class="price-match">
+                        <button><i class="mi mi-tags"></i> <?php echo __('We Price Match','tour-booking-manager'); ?></button>
                     </div>
                 </div>
             </div>
+            <div class="ttbm-hero-area">
+                <div class="slider-area">
+                    <?php do_action( 'ttbm_hotel_slider' ); ?>
+                </div>
+                <div class="review-map-container">
+                    <div class="review-container">
+                        <div class="review-rating">
+                            <div class="review">
+                                <h3>Excellant</h3>
+                                <p>787 reviews</p>
+                            </div>
+                            <div class="review-rate">
+                                7.8
+                            </div>
+                        </div>
+                        <div class="review-testimonial">
+                            <h2>Guests who stayed here loved</h2>
+                            <div class="testimonial">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            </div>
+                        </div>
+                        <div class="service-rating">
+                            <h3>Wifi</h3>
+                            <div class="service-rate">
+                                7.8
+                            </div>
+                        </div>
+                    </div>
+                    <div class="map-container">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d116833.97311724129!2d90.33711639865318!3d23.780818544256658!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b8b087026b81%3A0x8fa563bbdd5904c2!2sDhaka!5e0!3m2!1sen!2sbd!4v1754986559961!5m2!1sen!2sbd" width="100%" height="250px" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
             </div>
+            <div class="ttbm-content-area"></div>
+            <div class="ttbm-facilities-area"></div>
+            <div class="ttbm-booking-area"></div>
+            <div class="ttbm-review-area"></div>
+            <div class="ttbm-testimonial-area"></div>
+            <div class="ttbm-faq-area"></div>
         </div>
-        <?php do_action( 'ttbm_single_tour_after' ); ?>
     </div>
+    <?php do_action( 'ttbm_single_tour_after' ); ?>
+</div>
 <?php do_action( 'ttbm_related_tour' ); ?>

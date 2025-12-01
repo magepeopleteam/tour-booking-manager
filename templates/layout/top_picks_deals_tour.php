9571a6d31787277e7ac33673a704a28ff5d666e9
Shahadat <raselsha@gmail.com>
2025-12-01 10:57:55 +0600

    style: preloader and shortcode list grid

--- templates/layout/top_picks_deals_tour.php
+++ templates/layout/top_picks_deals_tour.php
@@ -32,7 +32,7 @@ if ( $related_tour_count > 0 && (TTBM_Global_Function::get_post_info( $ttbm_post
         <?php if( $carousel == 'no' ){ ?>
             <input type="hidden" id="ttbm_<?php echo esc_attr( $type_tour );?>_load_more_tour_shortcode" value="<?php echo esc_attr( $show )?>">
         <?php }?>
-        <div class="ttbm_container">
+        <div class="">
             <div class=''>
 <!--                --><?php //do_action( 'ttbm_section_title', 'ttbm_string_related_tour', esc_html__( ' '.ucfirst( $type_tour ).' Tour ', 'tour-booking-manager' ) ); ?>
                 <div class="ttbm_carousel_holder">
